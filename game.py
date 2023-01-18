117e1c7fabe7a8093b4ec6d616f8bfafed1b8ce3
Jiang <jiangyue0380@gmail.com>
2023-01-19 00:59:55 +1100

    updated add_blocks, add_players and create_game_board methods in Game class in game.py

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
+import json
+import time
+from property import Property
+from player import Player
+
+
 class Game:
     def __init__(self):
         self.players = []
         self.blocks = []
+        self.properties = []
         self.current_player = 0
         self.is_game_over = False
 
@@ -12,15 +19,48 @@ class Game:
 
     def add_blocks(self):
         # add blockes and properties on to the game board
-        pass
+        with open('board.json', 'r') as f:
+            board_data = f.read()
+            block_data = json.loads(board_data)
+
+        for block in block_data:
+            if block['name'] == "GO":
+                property_name = block['name']
+                self.blocks.append(
+                    Property(property_name, 0, 0, None))
+
+            else:
+                property_name = block['name']
+                property_cost = block['price']
+                property_rent = property_cost
+                property_colour = block['colour']
+                self.blocks.append(
+                    Property(property_name, property_cost, property_rent, property_colour))
+                self.properties.append(
+                    Property(property_name, property_cost, property_rent, property_colour))
 
     def add_players(self):
         # add players to the board
-        pass
+        self.players = [Player('Peter'), Player(
+            'Billy'), Player('Charlotte'), Player('Sweedal')]
 
     def create_game_board(self):
-        # display blocks on the board and players information
-        pass
+        # create game board with block and players information displayed
+        print('-----Generating the game board-----')
+        self.add_blocks()
+        time.sleep(2)
+        for i in range(len(self.blocks)):
+            print(f'Block Name: {self.blocks[i].name}\nBlock Position: {i}')
+            print('----------')
+            time.sleep(1)
+        print('-----Adding players to the game-----')
+        self.add_players()
+        time.sleep(2)
+        for i in range(len(self.players)):
+            print(
+                f'Player {i+1}: {self.players[i].name}\nCash ${self.players[i].cash}\nPosition: {self.players[i].position}')
+            print('----------')
+            time.sleep(1)
 
     def play_game(self):
         self.display_welcome_message_and_game_rules()
