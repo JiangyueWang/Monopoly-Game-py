9cc58ab4716697041378a38d16c35b172db5e7c0
Jiang <jiangyue0380@gmail.com>
2023-01-19 00:26:37 +1100

    created Property class in property.py

---
+++ property.py
@@ -0,0 +1,7 @@
+class Property:
+    def __init__(self, name, cost, rent, colour, owner=None):
+        self.name = name
+        self.cost = cost
+        self.rent = rent
+        self.colour = colour
+        self.owner = owner
