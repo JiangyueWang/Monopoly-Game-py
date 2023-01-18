40d889a0554585810b69a76e586b644cad14701f
Jiang <jiangyue0380@gmail.com>
2023-01-19 10:39:04 +1100

    created Dice class in dice.py + updated chosse_dice_and_roll and move methods for Player class in player.py

---
+++ dice.py
@@ -0,0 +1,10 @@
+import json
+
+
+class Dice:
+    def __init__(self, selected_dice_number):
+        self.dice_numbers = []
+        with open(f'rolls_{selected_dice_number}.json', 'r') as f:
+            dice_data = json.loads(f.read())
+            for i in dice_data:
+                self.dice_numbers.append(i)
--- player.py
+++ player.py
@@ -1,14 +1,31 @@
+from dice import Dice
+import random
+
+
 class Player:
     def __init__(self, name, cash=16, position=0):
         self.name = name
         self.cash = cash
         self.position = position
         self.properties_owned = []
+        self.number_of_moves = 0
 
     def choose_dice_and_roll(self):
-        pass
+        # ask player to choose which dice to roll
+        print('Please select a dice to roll')
+        self.player_select_dice = int(
+            input('Enter 1 for dice one, enter 2 for dice 2: '))
+        self.selected_dice = Dice(self.player_select_dice)
 
-    def move(self, spaces):
+        # randomly pick a number from player chosed cdice
+        self.index_of_dice_num = random.randint(
+            0, len(self.selected_dice.dice_numbers))
+        print(self.selected_dice.dice_numbers)
+        return self.selected_dice.dice_numbers[self.index_of_dice_num]
+
+    def move(self):
+        self.number_of_moves = self.choose_dice_and_roll()
+        self.position += self.number_of_moves
         pass
 
     def buy_property(self, property):
@@ -16,3 +33,9 @@ class Player:
 
     def pay_rent(self, property, amount):
         pass
+
+
+player = Player('Peter')
+print(player.position)
+player.move()
+print(player.position)
