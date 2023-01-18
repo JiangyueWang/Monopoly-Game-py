b1a2780fcc62d7037a80a5fb484e571f3d9f945a
Jiang <jiangyue0380@gmail.com>
2023-01-19 00:13:04 +1100

    created game preparation functions in the game.py

---
+++ game.py
@@ -0,0 +1,29 @@
+class Game:
+    def __init__(self):
+        self.players = []
+        self.blocks = []
+        self.current_player = 0
+        self.is_game_over = False
+
+    def display_welcome_message_and_game_rules(self):
+        # display welcome message
+        # display game rules
+        pass
+
+    def add_blocks(self):
+        # add blockes and properties on to the game board
+        pass
+
+    def add_players(self):
+        # add players to the board
+        pass
+
+    def create_game_board(self):
+        # display blocks on the board and players information
+        pass
+
+    def play_game(self):
+        self.display_welcome_message_and_game_rules()
+        self.create_game_board()
+        # game starts
+        pass
